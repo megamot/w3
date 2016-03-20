4fa2c7f2063748f014e08e78b49cd544a73a1efd
Artem But <Artem But>
2016-03-20 18:30:45 +0200

    Initial commit

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name w3App.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the w3App
+ */
+angular.module('w3App')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
